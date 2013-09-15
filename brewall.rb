4849a56a4ea5723b0f193f020de6dda074ff8307
rcmdnk <rcmdnk@gmail.com>
2013-09-15 16:18:29 +0200

    brewall.rb mytest.rb, from darwin11

--- brewall.rb
+++ brewall.rb
@@ -6,10 +6,7 @@ class Brewall < Formula
 
   head 'https://github.com/rcmdnk/homebrew-brewall.git', :branch => 'master'
 
-  skip_clean 'bin'
-
   def install
-    prefix.install 'bin'
     (bin+'brewall').chmod 0755
   end
 end
--- mytest.rb
+++ mytest.rb
@@ -1,12 +1,13 @@
 require 'formula'
 
-class Brewall < Formula
+class Mytest < Formula
   homepage 'https://github.com/rcmdnk/homebrew-mytest/'
   url 'https://github.com/rcmdnk/homebrew-mytest.git'
 
   head 'https://github.com/rcmdnk/homebrew-mytest.git', :branch => 'master'
 
   def install
-    (bin+'brewall').chmod 0755
+    prefix.install 'bin'
+    (bin+'mytest').chmod 0755
   end
 end
